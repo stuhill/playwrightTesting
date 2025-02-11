1a080fc3d58ebeb874230ab5c9da7226c2f1d014
stuhill <76552638+stuhill@users.noreply.github.com>
2025-02-11 13:51:07 +0000

    first commit

---
+++ tests/test1.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('https://sso.8x8pilot.com/v2/login');
+    await page.getByRole('textbox', { name: 'Username / Email' }).fill('s_hill3');
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Password123!');
+    await page.getByRole('button', { name: 'Login' }).click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByRole('link', { name: 'Configuration Manager for' }).click();
+    const page1 = await page1Promise;
+});
