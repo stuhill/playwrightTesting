82e3a72a5ca851c6e621befe097b5135c3259923
stuhill <76552638+stuhill@users.noreply.github.com>
2025-02-12 09:06:48 +0000

    removed unused step

--- tests/login-agui.spec.ts
+++ tests/login-agui.spec.ts
@@ -4,8 +4,7 @@ test('Log into AGUI', async ({ page }) => {
   await page.goto('https://sso.8x8pilot.com/v2/login');
   await page.getByRole('textbox', { name: 'Username / Email' }).fill('d50qamode3may.Agent.Two');
   await page.getByRole('button', { name: 'Continue' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Password123!');
   await page.getByRole('button', { name: 'Login' }).click();
   const page1Promise = page.waitForEvent('popup');
   await page.getByRole('link', { name: 'Contact Center Agent' }).click();
